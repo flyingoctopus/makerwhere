8bd4b7181ca1496a3ea16b3d9134cdc2d9cc255e
Vincent van Haaff <vincent@vanhaaff.com>
2011-06-04 19:47:31 +0000

    rails project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makerwhere::Application.config.secret_token = '598fe2df9217c280d714f4fc1f4823df0585eeddff41be077209e4c3ec18fc0b1351049bc4cab9fd91928f4a8aa4437baab05383647b35c8e5ef79d0bf030b1e'
